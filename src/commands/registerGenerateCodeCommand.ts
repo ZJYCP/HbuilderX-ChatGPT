68d4487564f65a9cf266da045f39710e3d0a026d
ly\LiYing <1975292055@qq.com>
2025-03-06 22:12:30 +0800

    feat: 代码内联补全

--- src/commands/registerGenerateCodeCommand.ts
+++ src/commands/registerGenerateCodeCommand.ts
@@ -1,28 +1,61 @@
-const hx = require("hbuilderx");
+const hx = require('hbuilderx');
+
+const fetchCode = async (language, fileName, prev, after) => {
+  const res = await fetch(
+    'http://backend.fcv3.1754953544581946.cn-hangzhou.fc.devsapp.net/llm/codeComplete',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        language,
+        fileName,
+        prev,
+        after,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    },
+  );
+  const code = res.text();
+  return code;
+};
 
 /**
- * 
- * @param context 
+ *
+ * @param context
  */
 export function registerGenerateCodeCommand(context) {
-    const inlineProvider = (document, position, context, token) => {
-        // 根据document和position确定上下文。
-        // 传递给语言分析工具，获取返回值
-        // 将返回值格式化成插件所需格式后返回。
-        console.log("ss");
+  let cancelToken;
+  const inlineProvider = async (document, position, context, token) => {
+    const language = document.languageId;
+    const fileName = document.fileName;
+    /**
+     * 根据position取得光标前和光标后的代码
+     */
+    // 获取光标前的代码
+    const codeBefore = document.getText(
+      new hx.Range(new hx.Position(0, 0), position),
+    );
 
-        return {
-            items: [{
-                insertText: `inse\nrtText`
-            }]
-        }
-    }
-    const InlineCompletionDisposable = hx.languages.registerInlineCompletionItemProvider(
-        [{
-            language: 'typescript',
-        }], {
-        provideInlineCompletionItems: inlineProvider
-    },
-    )
-    context.subscriptions.push(InlineCompletionDisposable)
+    // 获取光标后的代码
+    const lastLine = document.lineCount - 1;
+    const lastLineEnd = document.lineAt(lastLine).range.end;
+    const rangeAfterCusor = new hx.Range(position, lastLineEnd);
+    const codeAfter = document.getText(rangeAfterCusor);
+
+    const code = await fetchCode(language, fileName, codeBefore, codeAfter);
+
+    return {
+      items: [
+        {
+          insertText: code,
+        },
+      ],
+    };
+  };
+  const InlineCompletionDisposable =
+    hx.languages.registerInlineCompletionItemProvider([{ pattern: '**' }], {
+      provideInlineCompletionItems: inlineProvider,
+    });
+  context.subscriptions.push(InlineCompletionDisposable);
 }
